abc7b2d67e2643e80a4494a569b1510c25ada16c
Unnati Pulla <unnatipulla@Unnatis-MacBook-Pro-2.local>
2024-09-11 00:57:14 -0400

    unit tests added for Course class and bugs in Course noted in bugs.txt

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
@@ -1,4 +1,21 @@
 package dev.coms4156.project.individualproject;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains unit tests for Department class.
+ */
 public class DepartmentUnitTests {
+  @BeforeAll
+  public static void setupDepartmentForTesting() {
+  }
+
+  @Test
+  public void toStringTest() {
+
+  }
+
+  /** The test department instance used for testing. */
+  public static Department testDepartment;
 }
